96e69d08330d77dfdb4955ead08f00da77d8cb15
Ritka14 <daisydream.1408@gmail.com>
2024-01-26 16:37:48 +0200

    Add 3 Exercise

--- Hw1.Exercise3/GameApplication.cs
+++ Hw1.Exercise3/GameApplication.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Globalization;
+
+
 
 namespace Hw1.Exercise3
 {
@@ -25,6 +28,7 @@ namespace Hw1.Exercise3
             scissors,
             paper,
         };
+
         public enum Outcome
         {
             win,
@@ -32,13 +36,88 @@ namespace Hw1.Exercise3
             draw,
         };
 
+        public enum User
+        {
+            Player,
+            Computer,
+        };
+
         public int Run(string[] args)
         {
-            throw new NotImplementedException("Should be implemented by executor");
+            if (args == null || args.Length == 0 || args.Length > 1 || !Enum.TryParse(args[0].ToLowerInvariant(), out Game game))
+            {
+                return -1;
+            }
+
+            Random random = new Random();
+            var randomIndex = random.Next(Enum.GetValues(typeof(Game)).Length);
+            Game randomEnumValue = (Game)randomIndex;
+
+            Outcome resultPlayer = Outcome.draw;
+            Outcome resultComputer = Outcome.draw;
+            
+                switch (game)
+                {
+                    case Game.rock:
+                    {
+                        if (randomEnumValue == Game.scissors)
+                            resultPlayer = Outcome.win;
+                        else if (randomEnumValue == Game.paper)
+                            resultPlayer = Outcome.lose;
+                    }
+                        break;
+                    case Game.scissors:
+                    {
+                        if (randomEnumValue == Game.rock)
+                            resultPlayer = Outcome.lose;
+                        else if (randomEnumValue == Game.paper)
+                            resultPlayer = Outcome.win;
+                    }
+                        break;
+                    case Game.paper:
+                    {
+                        if (randomEnumValue == Game.rock)
+                            resultPlayer = Outcome.win;
+                        else if (randomEnumValue == Game.scissors)
+                            resultPlayer = Outcome.lose;
+                    }
+                        break;
+                }
+
+                if (resultPlayer == Outcome.win)
+                {
+                    resultComputer = Outcome.lose;
+                }
+                else if (resultPlayer == Outcome.lose)
+                {
+                    resultComputer = Outcome.win;
+                }
+                
+                string FirstCharToUpper(string input)
+                {
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        return input;
+                    }
+
+                    return char.ToUpperInvariant(input[0]) + input.Substring(1).ToLowerInvariant();
+                }
+                
+                Console.WriteLine($"{User.Player}={FirstCharToUpper(args[0])}:{FirstCharToUpper(resultPlayer.ToString())}");
+                Console.WriteLine($"{User.Computer}={FirstCharToUpper(randomEnumValue.ToString())}:{FirstCharToUpper(resultComputer.ToString())}");
+                
+
+                return 0;
         }
-
-
-
-
     }
+
 }
+
+
+
+
+
+
+
+
+
